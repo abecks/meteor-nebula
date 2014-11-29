ca796b4755ec0115b7efc5444065e0127391776f
Andrew Becker <abecks@gmail.com>
2014-11-29 11:44:13 -0800

    Add back routeHelpers.js

---
+++ client/helpers/routeHelpers.js
@@ -0,0 +1,47 @@
+var routeUtils = {
+  context: function() {
+    return Router.current();
+  },
+
+  regex: function(expression) {
+    return new RegExp(expression, 'i');
+  },
+
+  testRoutes: function(routeNames) {
+    var reg = this.regex(routeNames);
+    return this.context() && reg.test(this.context().route.name);
+  },
+
+  testPaths: function(paths) {
+    var reg = this.regex(paths);
+    return this.context() && reg.test(this.context().path);
+  }
+};
+
+Template.registerHelper('isActiveRoute', function(routes, className) {
+  if (className.hash)
+    className = 'active';
+
+  return routeUtils.testRoutes(routes) ? className : '';
+});
+
+Template.registerHelper('isActivePath', function(paths, className) {
+  if (className.hash)
+    className = 'active';
+
+  return routeUtils.testPaths(paths) ? className : '';
+});
+
+Template.registerHelper('isNotActiveRoute', function(routes, className) {
+  if (className.hash)
+    className = 'disabled';
+
+  return ! routeUtils.testRoutes(routes) ? className : '';
+});
+
+Template.registerHelper('isNotActivePath', function(paths, className) {
+  if (className.hash)
+    className = 'disabled';
+
+  return ! routeUtils.testPaths(paths) ? className : '';
+});
